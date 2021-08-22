8501fca184e33803e4fdbb36d8cffffc856e0041
osaretin <osaretin.frank10@gmail.com>
2021-08-22 17:44:44 +0100

    ready

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const hamburger = document.querySelector(".hamburger");
 const mobile = document.querySelector(".mobile-nav");
 
-console.log(mobile.classList)
 hamburger.addEventListener("click",(e)=>{
     if(mobile.className === "mobile-nav hide"){
         mobile.className = "mobile-nav show";
